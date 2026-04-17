38647fd21ec9089b8b725a671ab84a142b1e5b49
Anish Chaulagain <anishchaulagain2058@gmail.com>
2026-04-17 21:34:47 +0545

    day-02: create s3 bucket using terraform

---
+++ day-02/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "prod-bucket-s3-v1" {
+  bucket = "prod-bucket-s3-v1.0.0-1644234"
+
+  tags = {
+    Name        = "prod-bucket-s3-v1"
+    Environment = "production"
+  }
+}
