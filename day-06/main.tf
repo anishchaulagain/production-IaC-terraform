886dc70a43ff2055a3c15c6f5e4a610a0a7cab4b
Anish Chaulagain <anishchaulagain2058@gmail.com>
2026-04-18 21:15:13 +0545

    add foreach usage for provisioning multiple resource

--- day-06/main.tf
+++ day-06/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "prod-bucket-s3-v1" {
  count = 2
-  bucket = var.bucket_names[count.index]
+  bucket = var.bucket_names[count.index]  //also can use foreach = len(var.bucket_names) and bucket = var.bucket_names[each.key]
   tags = var.bucket_tag
 }
